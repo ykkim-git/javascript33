dc985222344bef98d4693261c697b040448d6a57
ykkim <44081957+ykkim-git@users.noreply.github.com>
2020-12-05 16:27:15 +0900

    19 / 33 map, reduce, filter

---
+++ 19.map, reduce, filter.js
@@ -0,0 +1,30 @@
+/**
+ * 1. map: 배열 내의 모든 요소 각각에 대하여 주어진 함수를 
+ 호출한 결과를 모아 새로운 배열로 반환한다.
+ */
+const mapArr = [1, 4, 9, 16];
+
+const map = mapArr.map(x => x * 2);
+
+console.log(map); // [2, 8, 18, 32]
+
+/**
+ * reduce(누산기acc, 현재값cur, 현재인덱스idx, 원본배열src)
+ 배열의 각 요소에 대해 주어진 리듀서 함수를 실행하고
+ 하나의 결과값을 반환한다.
+ */
+
+ const arr_reduce = [1, 2, 3, 4];
+ const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+ // 1+2+3+4
+ console.log(arr_reduce.reduce(reducer)); // 10
+
+ // 5+1+2+3+4
+ console.log(arr_reduce.reduce(reducer, 5));
+
+ /**
+  * recuce()는 하나의 콜백함수를 가지며
+  이 콜백함수는 이전값, 현재값(배열안의 요소)를 가지고 작업을 한다.
+  주로 누적된 값을 구할 때 많이 사용한다.
+  */
