906662f410cd23e77e5ca2cb48cd4f39771dd680
ykkim <44081957+ykkim-git@users.noreply.github.com>
2020-12-07 14:49:52 +0900

    filter 정리 추가

--- 19.map, reduce, filter.js
+++ 19.map, reduce, filter.js
@@ -9,7 +9,7 @@ const map = mapArr.map(x => x * 2);
 console.log(map); // [2, 8, 18, 32]
 
 /**
- * reduce(누산기acc, 현재값cur, 현재인덱스idx, 원본배열src)
+ * 2. reduce(누산기acc, 현재값cur, 현재인덱스idx, 원본배열src)
  배열의 각 요소에 대해 주어진 리듀서 함수를 실행하고
  하나의 결과값을 반환한다.
  */
@@ -28,3 +28,13 @@ console.log(map); // [2, 8, 18, 32]
   이 콜백함수는 이전값, 현재값(배열안의 요소)를 가지고 작업을 한다.
   주로 누적된 값을 구할 때 많이 사용한다.
   */
+
+
+
+  /**
+   * 3. filter: 주어진 함수의 테스트를 통과하는 모든 요소를 모아 새로운 배열로 리턴
+   */
+
+   const words = ['boolean', 'instanceof', 'string', 'number', 'null', 'undefined'];
+   const result = words.filter(word=> word.length > 6); // ["boolean", "instanceof", "undefined"]
+   console.log(result);
